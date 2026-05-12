cff958657fd1747abd81659aac17ec279588ca61
Rakh125 <166894547+DoItMark@users.noreply.github.com>
2026-05-13 00:19:15 +0700

    feat : change client api for cors

--- server/src/index.js
+++ server/src/index.js
@@ -13,7 +13,7 @@ async function main() {
   const app = express();
   app.use(cors(
     {
-      origin: process.env.CLIENT_URL || 'https://client-production-9793.up.railway.app',
+      origin: process.env.CLIENT_URL || 'https://client-production-c04a.up.railway.app',
       credentials: true,
     }
   ));
